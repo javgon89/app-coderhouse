5bc919de675eeb04532572ca654529e3bcbbc6d1
javgon89 <xjaviergonzalezx@gmail.com>
2021-01-18 23:19:30 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          App de <b>Javier González</b>
         </p>
         <a
           className="App-link"
