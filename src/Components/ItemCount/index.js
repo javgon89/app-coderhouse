5b0381040094ceca93d48eb9f52b49a1ba6b1c95
javgon89 <xjaviergonzalezx@gmail.com>
2021-01-31 21:25:13 -0300

    correcciones 2 entrega 4

--- src/Components/ItemCount/index.js
+++ src/Components/ItemCount/index.js
@@ -7,46 +7,45 @@ const ItemCount =({initial,stock}) =>{
     const [counter, setCounter] = useState(initial)
 
     const sumarProducto=()=>{
-        return counter<stock ? setCounter(counter+1):null
+        return counter<stock ? setCounter(counter+1) : null
     }
 
     const quitarProducto=()=>{
-        return counter>initial ? setCounter(counter-1):null
+        return counter>initial ? setCounter(counter-1) : null
     }
     
     console.log(counter)
 
     return(
         <>      
-                <div className="addProduct">        
-                    <p className="counterTitle">Agregar producto al carrito</p>
-                    <div className="counter">                    
-                        <Tooltip title="Quitar producto">
-                            <Button type="primary" 
-                                    shape="circle" 
-                                    onClick={quitarProducto} 
-                                    className="itemStyle" 
-                                    style={counter===initial ? {background:"#e4e4e4", border:"#bbbbbb", color:"#bbbbbb"}:{}}
-                                    >-
-                            </Button>
-                        </Tooltip>
+            <div className="addProduct">        
+                <p className="counterTitle">Agregar producto al carrito</p>
+                <div className="counter">                    
+                    <Tooltip title="Quitar producto">
+                        <Button type="primary" 
+                                shape="circle" 
+                                onClick={quitarProducto} 
+                                className="itemStyle" 
+                                style={counter===initial ? {background:"#e4e4e4", border:"#bbbbbb", color:"#bbbbbb"}:{}}
+                                >-
+                        </Button>
+                    </Tooltip>
 
-                        <p className="numberStyle">{counter}</p>   
+                    <p className="numberStyle">{counter}</p>   
 
-                        <Tooltip title={counter===stock ? "No hay más unidades disponibles" : "Agregar producto al carrito"}>
-                            <Button type="primary" 
-                                    shape="circle" 
-                                    onClick={sumarProducto} 
-                                    className="itemStyle" 
-                                    style={counter===stock ? {background:"#e4e4e4", border:"#bbbbbb", color:"#bbbbbb"}:{}}>+
-                            </Button>
-                        </Tooltip>
-                    </div>
-                    <p className="avUnits">Unidades disponibles: {stock}</p>
-                    <Button type="primary" className="mainButton">+ Agregar al carrito</Button>
+                    <Tooltip title={counter===stock ? "No hay más unidades disponibles" : "Agregar producto al carrito"}>
+                        <Button type="primary" 
+                                shape="circle" 
+                                onClick={sumarProducto} 
+                                className="itemStyle" 
+                                style={counter===stock ? {background:"#e4e4e4", border:"#bbbbbb", color:"#bbbbbb"}:{}}>+
+                        </Button>
+                    </Tooltip>
                 </div>
-
-            </>
+                <p className="avUnits">Unidades disponibles: {stock}</p>
+                <Button type="primary" className="mainButton">+ Agregar al carrito</Button>
+            </div>
+        </>
         )
 
 }
